476fb5d5f96b7ba15c6a0f479385fce1e00f2cb2
Carolina Ciampini <carolinaciampini02@gmail.com>
2024-03-02 15:48:50 -0300

    css y html listo

---
+++ js/main.js
@@ -0,0 +1,22 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navBar = document.querySelector(".navbar");
+
+let sections = document.querySelectorAll("section")
+let navLinks = document.querySelectorAll("header nav a")
+/*
+
+function scrollToSection(sectionId) {
+        const section = document.getElementById(sectionId);
+
+        if (section) {
+            section.scrollIntoView({ behavior: 'smooth' });
+        }
+}
+    
+document.addEventListener("DOMContentLoaded", function (section) {
+    
+   scrollToSection(section)
+
+    
+});
+*/
